8bfdd951f6e01875e2df5aefa34b7d6aa4e5d8b7
CodeThane <Giftthane551@gmail.com>
2024-07-29 11:56:03 +0200

    Bridge Labs Section2

---
+++ GiftWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using GiftWebApp.Models;
+
+namespace GiftWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<GiftWebApp.Models.Gift> Gift { get; set; } = default!;
+    }
+}
---
+++ GiftWebApp/Models/Gift.cs
@@ -0,0 +1,14 @@
+﻿namespace GiftWebApp.Models
+{
+    public class Gift
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Gift()
+        {
+            
+        }
+    }
+}
